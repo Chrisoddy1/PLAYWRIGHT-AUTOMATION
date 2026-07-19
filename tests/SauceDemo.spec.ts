abf890175717c08540a1fbb3bbb8a343d3eb8be3
chrisoddy1 <taiwo.odeniyi@yhoo.com>
2026-07-19 05:44:15 +0100

    books

--- tests/SauceDemo.spec.ts
+++ tests/SauceDemo.spec.ts
@@ -4,5 +4,10 @@ test('Sauce Demo Login Test', async ({ page }) => {
     await page.goto('https://www.saucedemo.com/')
     await page.locator("//*[@id ='user-name']").fill('standard_user')
     await page.locator(" //input[@id ='password']").fill('secret_sauce')
-    await page.locator("//input[@id ='login-button']").click()
+    //await page.locator("//input[@id ='login-button']").click()
+    await page.locator("text=Login").click()
+    await page.locator("text=Sauce Labs Backpack").click()
+     await page.locator("text='Add to cart'").click()
+      await page.locator("text=REmove").click()
+    
 })
