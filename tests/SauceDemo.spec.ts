02bf3fdc28635923165d9a857dfba54903125382
chrisoddy1 <taiwo.odeniyi@yhoo.com>
2026-07-20 23:22:00 +0100

    dedf

--- tests/SauceDemo.spec.ts
+++ tests/SauceDemo.spec.ts
@@ -8,9 +8,25 @@ test('Sauce Demo Login Test', async ({ page }) => {
 	await page.locator(" //input[@id ='password']").fill('secret_sauce');
 	//await page.locator("//input[@id ='login-button']").click()
 	await page.locator('text=Login').click();
-	await page.locator('text=Sauce Labs Backpack').click();
+	//await page.locator('.inventory_item_name', { hasNotText: /Sauce1.*/ }).click();
+	const items = await page.locator('.inventory_item_name').allTextContents();
+	console.log(items);
+	await page.getByRole('button', { name: 'frgg' }).click();
+	// await page.locator('text=Sauce Labs Backpack').click();
+	await page.locator('ffbfbbfb').click();
 
-	await page.locator("text='Add to cart'").click();
+	// await page.locator("text='Add to cart'").click();
 
 	await page.locator('text=REMOVE').click();
+});
+
+test('second test', async ({ page }) => {
+	page.goto('https://www.saucedemo.com/');
+	await page.getByPlaceholder('Username').fill('standard_user');
+	await page.locator(" //input[@id ='password']").fill('secret_sauce');
+	await page.locator('text=Login').click();
+	await page.locator('#react-burger-menu-btn').click();
+	//await page.locator('hdhdhdhd').click();
+	 await page.goto('saucedemo.com');
+
 });
---
+++ tests/VarificationTest.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('verify Login button count', async ({ page }) => {
+	await page.goto('https://www.saucedemo.com/');
+
+	await expect(page.getByRole('button', { name: 'Login' })).toHaveCount(1);
+	await expect (page.getByPlaceholder('Username')).toHaveCount(1)
+});
